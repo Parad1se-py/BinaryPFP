29cbac6c15c2019e18c6eebd77c0500200e0e67a
Arjun Ladha <tigerninja2234@gmail.com>
2021-12-21 21:42:14 +0530

    change in create_pos_lists from funcs.py

--- binary_pfp/pos.py
+++ binary_pfp/pos.py
@@ -1,4 +1,6 @@
 import random
 
 def create_pos_lists():
-    return [(x:=[random.randint(0, 1) for _ in range(4)])+x[::-1] for _ in range(8)]
+    return [
+        (x:=[random.randint(0, 1) for _ in range(4)])+x[::-1] for _ in range(8)
+    ]
